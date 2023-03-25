e9fcdf70ecabb4fc95a5a81cec6b1299e5b0f05a
brandon <brandon@aquacyber>
2023-03-25 14:16:50 +0300

    First Commit Brandon

---
+++ src/main/java/com/omnera/nerapp/Masomo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.omnera.nerapp;
+
+/**
+ *
+ * @author brandon
+ */
+public class Masomo {
+    
+    Masomo(int cap)
+    {
+        this.Mania(cap);
+    }
+    public int AnzaMasomo()
+    {
+        return 0;
+    }
+    
+    public void Mania(int theNumber)
+    {
+        System.out.println("The Real value is " +theNumber+ " end of topic!");
+    }
+}
--- src/main/java/com/omnera/nerapp/Nerapp.java
+++ src/main/java/com/omnera/nerapp/Nerapp.java
@@ -14,10 +14,14 @@ public class Nerapp {
         //int numero = 8892;
         //double numero2 = 77.3423;
         //System.out.println("Welcome to the Dynasty! " +numero2);
-    
+    /*
         System.out.println("First Line\nSecond Line\nThird Line Hommie");
         System.out.println("A\tB\tC");
         System.out.println("D\tE\tF");
         System.out.println("The Omnera\tCorporation of\tKenya");
+        */
+    
+    Masomo amasomo = new Masomo(67);
+    
     }
 }
