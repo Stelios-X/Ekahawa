b9b66438c5357ea373e07d1617e077c9103992a3
brandon <brandon@aquacyber>
2023-03-25 13:42:32 +0300

    First Commit Brandon

---
+++ src/main/java/com/omnera/nerapp/Nerapp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.omnera.nerapp;
+
+/**
+ *
+ * @author brandon
+ */
+public class Nerapp {
+
+    public static void main(String[] args) {
+        //int numero = 8892;
+        //double numero2 = 77.3423;
+        //System.out.println("Welcome to the Dynasty! " +numero2);
+    
+        System.out.println("First Line\nSecond Line\nThird Line Hommie");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+        System.out.println("The Omnera\tCorporation of\tKenya");
+    }
+}
